b7dddafa256ab9d3f4d469b28a432b65c87a8ea8
coreyhaines <coreyhaines@gmail.com>
2012-02-15 12:29:18 -0600

    Look, I got rails setup!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapTest::Application.config.secret_token = 'df4c900f90a9e75d8fc9e4a3339e9233e496ee180f0efcb3b55aa8665bd41721ceab87b8566860509ec37e6387c10087b999194eba0de5ff61a217b6349b2071'
